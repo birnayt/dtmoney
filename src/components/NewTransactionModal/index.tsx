445c694a45cd07aeb28d39fdf7c3a9afb4114c6b
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-08-01 23:01:43 -0300

    feat: finish style NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
 
+import closeImg from '../../assets/close.svg';
+
+import * as S from './styles';
+
 interface NewTransactionModalProps {
   isOpenNewTransactionModal: boolean;
   handleCloseNewTransactionModal: () => void;
@@ -11,10 +15,38 @@ export function NewTransactionModal({
 }: NewTransactionModalProps) {
   return (
     <Modal
-    isOpen={isOpenNewTransactionModal}
-    onRequestClose={handleCloseNewTransactionModal}
+      isOpen={isOpenNewTransactionModal}
+      onRequestClose={handleCloseNewTransactionModal}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Modal de transações</h2>
+      <button 
+       type="button" 
+       onClick={handleCloseNewTransactionModal} 
+       className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <S.Container>
+        <h2>Cadastrar transação</h2>
+
+        <input
+         placeholder="Título"
+        />
+
+        <input
+         placeholder="Valor"
+        />
+
+        <input
+         placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </S.Container>
     </Modal>
   );
 }
